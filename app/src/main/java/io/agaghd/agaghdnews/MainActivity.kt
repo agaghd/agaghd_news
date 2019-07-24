98529271448ddd9af68735210dbbb20984c3122e
agaghd <cover_story@163.com>
2019-07-24 21:31:02 +0800

    加入recyclerview和bravh

--- app/src/main/java/io/agaghd/agaghdnews/MainActivity.kt
+++ app/src/main/java/io/agaghd/agaghdnews/MainActivity.kt
@@ -1,47 +1,13 @@
 package io.agaghd.agaghdnews
 
 import android.os.Bundle
-import io.agaghd.agaghdnews.network.RequestListener
-import io.agaghd.agaghdnews.network.RequestUtil
 import io.agaghd.agaghdnews.utils.BaseActivity
-import io.agaghd.agaghdnews.utils.ToastUtil
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        RequestUtil.getChannel(object : RequestListener {
-            override fun onSuccess(reponse: String) {
-                hello_world_tv.append("\n" + reponse + "\n")
-            }
-
-            override fun onError(msg: String) {
-                ToastUtil.showToast(mContext, msg)
-            }
-
-        })
-        RequestUtil.getNewsByChannel("财经", 10, 0, object : RequestListener {
-            override fun onSuccess(reponse: String) {
-                hello_world_tv.append("\n" + reponse + "\n")
-            }
-
-            override fun onError(msg: String) {
-                ToastUtil.showToast(mContext, msg)
-            }
-
-        })
-        RequestUtil.newSearch("科创板", object : RequestListener {
-            override fun onSuccess(reponse: String) {
-                hello_world_tv.append("\n" + reponse + "\n")
-            }
-
-            override fun onError(msg: String) {
-                ToastUtil.showToast(mContext, msg)
-            }
-
-        })
     }
 
 }
--- app/src/main/java/io/agaghd/agaghdnews/SplashActivity.kt
+++ app/src/main/java/io/agaghd/agaghdnews/SplashActivity.kt
@@ -1,13 +1,25 @@
 package io.agaghd.agaghdnews
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        getVersionInfo()
+    }
+
+    fun getVersionInfo() {
+        //TODO 获取版本信息和升级
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, 2500)
+
     }
 
 
